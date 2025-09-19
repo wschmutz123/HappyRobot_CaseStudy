6e6d8f7c76a0af94c6ee460029d4d09ee846d713
Will Schmutz <will.schmutz@ibm.com>
2025-09-19 15:01:37 -0700

    update port

--- Backend/main.py
+++ Backend/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,  # points to this file
         host="0.0.0.0",
-        port=443,
+        port=3001,
         ssl_keyfile="/etc/ssl/private/selfsigned.key",
         ssl_certfile="/etc/ssl/certs/selfsigned.crt"
     )
