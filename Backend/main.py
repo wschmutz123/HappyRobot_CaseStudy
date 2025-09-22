c59a2928792b20004a725fde3138654e37811390
Will Schmutz <will.schmutz@ibm.com>
2025-09-22 08:18:18 -0700

    modify route

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,17 @@
 # main.py
 import uvicorn
 from app.main import app
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FULLCHAIN_DIR = os.path.join(BASE_DIR, "certs", "fullchain.pem")
+PRIVKEY_DIR = os.path.join(BASE_DIR, "certs", "privkey.pem")
 
 if __name__ == "__main__":
     uvicorn.run(
         app,  # points to this file
         host="0.0.0.0",
         port=3001,
-        ssl_certfile="/certs/fullchain.pem",
-        ssl_keyfile="/certs/privkey.pem"
+        ssl_certfile=FULLCHAIN_DIR,
+        ssl_keyfile=PRIVKEY_DIR
     )
